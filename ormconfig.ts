84010ebe669df329535e7cdecdc19c4a83bbcb5e
Jean <jean-cabral@hotmail.com>
2022-05-26 03:12:41 -0300

    testando melhor forma de fazer migrations com typeorm

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+
+export const connectionSource = new DataSource({
+  migrationsTableName: 'migrations',
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'user',
+  password: 'pass',
+  database: 'somehealthchecker',
+  logging: false,
+  synchronize: false,
+  name: 'default',
+  entities: ['src/**/**.entity{.ts,.js}'],
+  migrations: ['src/migrations/**/*{.ts,.js}'],
+  subscribers: ['src/subscriber/**/*{.ts,.js}'],
+});
